38cc3607fc7530134ee34c0a0874aec49c62037d
programadorforocoches <fghj3693fghj3693@gmail.com>
2021-07-22 15:46:46 +0100

    primeras lineas de codigo en java

---
+++ conversacion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class conversacion {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("cual es tu nombre");
+        String nombre = entrada.nextLine();
+        System.out.println("tu nombre es " + nombre);
+
+
+    }
+
+}
+
