cd4797acaea1b87abd9f9a511d66c42fd0b93e0c
avnavas <avnavas65@gmail.com>
2021-11-13 14:34:41 +0100

    recorrer un array y mostrar en consola independientemente de su longitud

---
+++ pruebaArray.java
@@ -0,0 +1,19 @@
+public class pruebaArray {
+    public static void main(String[] args) {
+
+        int [] mi_matriz = new int [5];
+
+        mi_matriz [0] = 5;
+        mi_matriz [1] = 3;
+        mi_matriz [2] = 4;
+        mi_matriz [3] = 7;
+        mi_matriz [4] = 9;
+
+
+        for (int i=0; i < mi_matriz.length; i++) {
+            System.out.println(mi_matriz[i]);
+        }
+
+    }
+
+}
