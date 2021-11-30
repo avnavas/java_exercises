05ee19e87dacfaa8ccc30fe50c568a1acc4b375b
avnavas <avnavas65@gmail.com>
2021-11-30 18:14:01 +0100

    clases y objetos

---
+++ poo/Coche.java
@@ -0,0 +1,25 @@
+package poo;
+
+
+public class Coche {
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+
+
+
+    public Coche (){
+        ruedas = 4;
+        largo = 400;
+        ancho = 200;
+        motor = 1600;
+        peso = 2000;
+
+    }
+
+
+}
+
+
---
+++ poo/UsoCoche.java
@@ -0,0 +1,9 @@
+package poo;
+
+public class UsoCoche {
+    public static void main(String[] args) {
+        Coche renault = new Coche();
+
+        System.out.println(renault.ruedas);
+    }
+}
